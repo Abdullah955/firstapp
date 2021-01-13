49534c12e2d341633de939ae2ea4a393fed6d5a7
Abdullah955 <abdullahalatter@gmail.com>
2021-01-13 22:04:11 +0300

    Initial Commit

---
+++ Pathy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pathy
+//
+//  Created by Abdullah Alatter on 13/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
