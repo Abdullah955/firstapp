983602da807e228f77bde95d6d7e8e1818ef79cd
Abdullah955 <abdullahalatter@gmail.com>
2021-01-20 19:22:13 +0300

    updating Logout

--- Pathy/Controllers/ProfileViewController.swift
+++ Pathy/Controllers/ProfileViewController.swift
@@ -11,9 +11,23 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        ConfigurNavigationBar()
 
-        // Do any additional setup after loading the view.
     }
     
 
+    private func ConfigurNavigationBar(){
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"),
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didtapSettingButton))
+           
+    }
+    
+    @objc private func didtapSettingButton(){
+        let vc = SettingViewController()
+        vc.title = "Settings"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
