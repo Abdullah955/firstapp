eb5314cad8dfca4b4ff88334071f05ac9c9c4d0a
Abdullah955 <abdullahalatter@gmail.com>
2021-01-20 18:38:38 +0300

    adding Setting Page

--- Pathy/Controllers/SettingViewController.swift
+++ Pathy/Controllers/SettingViewController.swift
@@ -10,28 +10,35 @@ import UIKit
 class SettingViewController: UIViewController {
 
     
-    public let tableView: UITableView = {
-        
-    let tableView = UITableView()
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero,style: .grouped)
+        tableView.register(UITableViewCell.self , forCellReuseIdentifier: "cell" )
         return tableView
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
-    */
 
+}
+
+extension SettingViewController: UITableViewDelegate,UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
 }
